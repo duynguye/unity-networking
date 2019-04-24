54cab38d8e4ef62df27566508a828e7db92a06b1
Andy Nguyen <andy@tier3interactive.com>
2019-04-24 08:22:37 -0400

    Initial commit

---
+++ Assets/Scripts/ClientBehavior.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+
+using Unity.Networking.Transport;
+using Unity.Networking.Transport.Utilities;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+
+using NetworkConnection = Unity.Networking.Transport.NetworkConnection;
+
+public class ClientBehavior : MonoBehaviour
+{
+    public UdpNetworkDriver m_Driver;
+    public NetworkPipeline m_Pipeline;
+    public NetworkConnection m_Connection;
+    public NetworkEndPoint m_Endpoint;
+    public bool Done;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Driver = new UdpNetworkDriver(new SimulatorUtility.Parameters { MaxPacketSize = 256, MaxPacketCount = 30, PacketDelayMs = 100 });
+        m_Pipeline = m_Driver.CreatePipeline(typeof(UnreliableSequencedPipelineStage), typeof(SimulatorPipelineStage));
+        m_Connection = default;
+
+        m_Endpoint = new NetworkEndPoint();
+        m_Endpoint = NetworkEndPoint.Parse("127.0.0.1", 9000);
+
+        m_Connection = m_Driver.Connect(m_Endpoint);
+    }
+
+    public void OnDestroy()
+    {
+        m_Driver.Dispose();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Driver.ScheduleUpdate().Complete();
+
+        if (!m_Connection.IsCreated)
+        {
+            if (!Done)
+            {
+                Debug.Log("Something went wrong during connection");
+            }
+
+            return;
+        }
+
+        DataStreamReader stream;
+        NetworkEvent.Type cmd;
+
+        while ((cmd = m_Connection.PopEvent(m_Driver, out stream)) != NetworkEvent.Type.Empty)
+        {
+            if (cmd == NetworkEvent.Type.Connect)
+            {
+                Debug.Log("We are now connected to the server.");
+
+                var value = 1;
+
+                using (var writer = new DataStreamWriter(4, Allocator.Temp))
+                {
+                    writer.Write(value);
+                    m_Connection.Send(m_Driver, writer);
+                }
+            }
+            else if (cmd == NetworkEvent.Type.Data)
+            {
+                var readerCtx = default(DataStreamReader.Context);
+                uint value = stream.ReadUInt(ref readerCtx);
+
+                Debug.Log("Got the value = " + value + " back from the server.");
+
+                Done = true;
+                m_Connection.Disconnect(m_Driver);
+                m_Connection = default;
+            }
+            else if (cmd == NetworkEvent.Type.Disconnect)
+            {
+                Debug.Log("Client got disconnected from the server.");
+                m_Connection = default;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ServerBehavior.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+
+using Unity.Networking.Transport;
+using Unity.Networking.Transport.Utilities;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+
+using NetworkConnection = Unity.Networking.Transport.NetworkConnection;
+
+public class ServerBehavior : MonoBehaviour
+{
+    public UdpNetworkDriver m_Driver;
+    public NetworkPipeline m_Pipeline;
+    public NetworkEndPoint m_Endpoint;
+    private NativeList<NetworkConnection> m_Connections;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        m_Driver = new UdpNetworkDriver(new SimulatorUtility.Parameters { MaxPacketSize = 256, MaxPacketCount = 30, PacketDelayMs = 100 });
+        m_Pipeline = m_Driver.CreatePipeline(typeof(UnreliableSequencedPipelineStage), typeof(SimulatorPipelineStage));
+
+        m_Endpoint = new NetworkEndPoint();
+        m_Endpoint = NetworkEndPoint.Parse("0.0.0.0", 9000);
+
+        if (m_Driver.Bind(m_Endpoint) != 0)
+        {
+            Debug.Log("Failed to bind to port 9000");
+        }
+        else
+        {
+            m_Driver.Listen();
+        }
+
+        m_Connections = new NativeList<NetworkConnection>(16, Allocator.Persistent);
+    }
+
+    private void OnDestroy()
+    {
+        m_Driver.Dispose();
+        m_Connections.Dispose();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        m_Driver.ScheduleUpdate().Complete();
+
+        // Clean up connections
+        for (int i = 0; i < m_Connections.Length; i++)
+        {
+            if (!m_Connections[i].IsCreated)
+            {
+                m_Connections.RemoveAtSwapBack(i);
+                --i;
+            }
+        }
+
+        // Accept new connections
+        NetworkConnection connection;
+
+        while ((connection = m_Driver.Accept()) != default(NetworkConnection))
+        {
+            m_Connections.Add(connection);
+            Debug.Log("Accepted a connection");
+        }
+
+        DataStreamReader stream;
+        for (int i = 0; i < m_Connections.Length; i++)
+        {
+            if (!m_Connections[i].IsCreated)
+            {
+                continue;
+            }
+
+            NetworkEvent.Type cmd;
+
+            while ((cmd = m_Driver.PopEventForConnection(m_Connections[i], out stream)) != NetworkEvent.Type.Empty)
+            {
+                if (cmd == NetworkEvent.Type.Data)
+                {
+                    var readerCtx = default(DataStreamReader.Context);
+                    uint number = stream.ReadUInt(ref readerCtx);
+
+                    Debug.Log("Got " + number + " from the client adding + 2 to it and sending it back.");
+
+                    number += 2;
+
+                    using (var writer = new DataStreamWriter(4, Allocator.Temp))
+                    {
+                        writer.Write(number);
+                        m_Driver.Send(m_Pipeline, m_Connections[i], writer);
+                    }
+                }
+                else if (cmd == NetworkEvent.Type.Disconnect)
+                {
+                    Debug.Log("Client disconnected from the server.");
+                    m_Connections[i] = default;
+                }
+            }
+        }
+    }
+}
